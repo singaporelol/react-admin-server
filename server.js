a775aa67e1fa185a891f6a460910e7f43fca39ce
xueqian-Destop <345589726@qq.com>
2022-07-19 23:50:40 +0800

    Added: change port to 3101

--- server.js
+++ server.js
@@ -5,34 +5,33 @@ const router = jsonServer.router(db);
 const multer = require("multer");
 const path = require("path");
 
-
 const middlewares = jsonServer.defaults({
-  watch: true,
-  // noCors: true,
+	watch: true,
+	// noCors: true,
 });
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    console.log(__dirname);
-    cb(null, path.join(__dirname, "public/upload"));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
+	destination: function (req, file, cb) {
+		console.log(__dirname);
+		cb(null, path.join(__dirname, "public/upload"));
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(null, uniqueSuffix + "-" + file.originalname);
+	},
 });
 const upload = multer({ storage: storage });
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.all(
-  "/upload/avatar",
-  upload.single("avatar"),
-  function (req, res, next) {
-    console.log(req.file);
-    console.log(req.body);
-    res.json({ img: `/upload/${req.file.filename}` });
-  }
+	"/upload/avatar",
+	upload.single("avatar"),
+	function (req, res, next) {
+		console.log(req.file);
+		console.log(req.body);
+		res.json({ img: `/upload/${req.file.filename}` });
+	}
 );
 
 /**
@@ -45,76 +44,76 @@ server.all(
 //手动处理many-to-many的情况：
 //get roles by username ids
 server.get("/api/roles", function (req, res, next) {
-  const ids = req.query.ids;
+	const ids = req.query.ids;
 
-  let user_roleList = ids.reduce((pre, cur) => {
-    return [...pre, ...db.user_role.filter((u) => u.userId == cur)];
-  }, []);
-  let roleList = user_roleList.reduce((pre, cur) => {
-    return [...pre, ...db.roles.filter((r) => r.id == cur.roleId)];
-  }, []);
-  res.jsonp({
-    roleList,
-  });
+	let user_roleList = ids.reduce((pre, cur) => {
+		return [...pre, ...db.user_role.filter((u) => u.userId == cur)];
+	}, []);
+	let roleList = user_roleList.reduce((pre, cur) => {
+		return [...pre, ...db.roles.filter((r) => r.id == cur.roleId)];
+	}, []);
+	res.jsonp({
+		roleList,
+	});
 });
 //根据角色返回对应并且格式化好的权限
 server.get("/api/permissions", (req, res, next) => {
-  const ids = req.query.ids;
-  let role_permissionList = ids.reduce((pre, cur) => {
-    return [...pre, ...db.role_permission.filter((r) => r.roleId == cur)];
-  }, []);
-  let permissionList = role_permissionList.reduce((pre, cur) => {
-    return [...pre, ...db.permissions.filter((p) => p.id == cur.permissionId)];
-  }, []);
+	const ids = req.query.ids;
+	let role_permissionList = ids.reduce((pre, cur) => {
+		return [...pre, ...db.role_permission.filter((r) => r.roleId == cur)];
+	}, []);
+	let permissionList = role_permissionList.reduce((pre, cur) => {
+		return [...pre, ...db.permissions.filter((p) => p.id == cur.permissionId)];
+	}, []);
 
-  res.json({
-    permissionList,
-  });
+	res.json({
+		permissionList,
+	});
 });
 
 server.get("/api/permissions_treeview", (req, res, next) => {
-  let permissionList = formatPermissionList(db.permissions);
-  res.json({
-    permissionList,
-  });
+	let permissionList = formatPermissionList(db.permissions);
+	res.json({
+		permissionList,
+	});
 });
 
 server.post("/api/userlogin", (req, res, next) => {
-  let { username, password } = req.body;
-  // console.log(req.body);
-  let user = db.users.find((item) => {
-    return item.username == username && item.password == password;
-  });
-  if (user) {
-    //登录成功拿到用户角色，权限
-    //拿到关联用户的角色id
-    let roleIdsList = db.user_role.reduce((pre, cur) => {
-      if (cur.userId == user.id) {
-        pre.push(cur.roleId);
-      }
-      return pre;
-    }, []);
+	let { username, password } = req.body;
+	// console.log(req.body);
+	let user = db.users.find((item) => {
+		return item.username == username && item.password == password;
+	});
+	if (user) {
+		//登录成功拿到用户角色，权限
+		//拿到关联用户的角色id
+		let roleIdsList = db.user_role.reduce((pre, cur) => {
+			if (cur.userId == user.id) {
+				pre.push(cur.roleId);
+			}
+			return pre;
+		}, []);
 
-    //根据关联的角色ID拿到这些角色所对应的所有权限ID
-    let permissionIdsAllList = db.role_permission.reduce((pre, cur) => {
-      if (roleIdsList.indexOf(cur.roleId) >= 0) {
-        pre.push(cur.permissionId);
-      }
-      return pre;
-    }, []);
-    console.log(permissionIdsAllList);
-    //对多个角色都有的权限去重
-    let permissionIdsList = [...new Set(permissionIdsAllList)];
-    //根据权限ID，拿到所有的权限
-    let userPermissionList = permissionIdsList.reduce((pre, cur) => {
-      pre.push(db.permissions.find((u) => u.id == cur));
-      return pre;
-    }, []);
-    console.log(userPermissionList);
-    let userPagePermissionList = userPermissionList.filter((u) => u.type == 1);
-    let btnPermission = userPermissionList.filter((u) => u.type == 2);
-    //格式化permissionList
-    /**
+		//根据关联的角色ID拿到这些角色所对应的所有权限ID
+		let permissionIdsAllList = db.role_permission.reduce((pre, cur) => {
+			if (roleIdsList.indexOf(cur.roleId) >= 0) {
+				pre.push(cur.permissionId);
+			}
+			return pre;
+		}, []);
+		console.log(permissionIdsAllList);
+		//对多个角色都有的权限去重
+		let permissionIdsList = [...new Set(permissionIdsAllList)];
+		//根据权限ID，拿到所有的权限
+		let userPermissionList = permissionIdsList.reduce((pre, cur) => {
+			pre.push(db.permissions.find((u) => u.id == cur));
+			return pre;
+		}, []);
+		console.log(userPermissionList);
+		let userPagePermissionList = userPermissionList.filter((u) => u.type == 1);
+		let btnPermission = userPermissionList.filter((u) => u.type == 2);
+		//格式化permissionList
+		/**
      {
         id: 1,
         name: "用户管理",
@@ -134,128 +133,128 @@ server.post("/api/userlogin", (req, res, next) => {
       },
      */
 
-    let result = formatPermissionList(userPagePermissionList);
-    console.log(result);
-    // user.allPermission = result;
-    user = { ...user, allPermission: result, btnPermission };
-    res.jsonp({
-      user,
-      code: 1,
-      msg: "login success",
-      token: Date.now() + "",
-    });
-  } else {
-    res.json({
-      code: 0,
-      msg: "Username or password is wrong!",
-    });
-  }
-  // next();
+		let result = formatPermissionList(userPagePermissionList);
+		console.log(result);
+		// user.allPermission = result;
+		user = { ...user, allPermission: result, btnPermission };
+		res.jsonp({
+			user,
+			code: 1,
+			msg: "login success",
+			token: Date.now() + "",
+		});
+	} else {
+		res.json({
+			code: 0,
+			msg: "Username or password is wrong!",
+		});
+	}
+	// next();
 });
 server.put("/api/user_role/:userId", (req, res, next) => {
-  let userId = +req.params.userId;
-  let roleIdList = req.body.selectRoles;
-  console.log(roleIdList);
-  let newRoles = roleIdList.map((u, index) => {
-    return { id: Date.now() + index, userId, roleId: u };
-  });
-  console.log(newRoles);
-  db.user_role = db.user_role
-    .filter((u) => u.userId != userId)
-    .concat(newRoles);
-  res.json({
-    code: 1,
-  });
+	let userId = +req.params.userId;
+	let roleIdList = req.body.selectRoles;
+	console.log(roleIdList);
+	let newRoles = roleIdList.map((u, index) => {
+		return { id: Date.now() + index, userId, roleId: u };
+	});
+	console.log(newRoles);
+	db.user_role = db.user_role
+		.filter((u) => u.userId != userId)
+		.concat(newRoles);
+	res.json({
+		code: 1,
+	});
 });
 server.put("/api/role_permission/:roleId", (req, res, next) => {
-  let roleId = +req.params.roleId;
-  let permissionIdList = req.body.selectPermissions;
-  let newPermissions = permissionIdList.map((u, index) => {
-    return { id: Date.now() + index, roleId, permissionId: u };
-  });
-  db.role_permission = db.role_permission
-    .filter((u) => u.roleId != roleId)
-    .concat(newPermissions);
-  res.json({
-    code: 1,
-  });
+	let roleId = +req.params.roleId;
+	let permissionIdList = req.body.selectPermissions;
+	let newPermissions = permissionIdList.map((u, index) => {
+		return { id: Date.now() + index, roleId, permissionId: u };
+	});
+	db.role_permission = db.role_permission
+		.filter((u) => u.roleId != roleId)
+		.concat(newPermissions);
+	res.json({
+		code: 1,
+	});
 });
 //删除用户表和用户角色表
 server.delete("/api/users/:id", (req, res, next) => {
-  let userId = req.params.id;
-  console.log(userId);
-  db.user_role = db.user_role.filter((u) => u.userId != userId);
-  db.users = db.users.filter((u) => u.id != userId);
-  res.json({
-    code: 1,
-  });
+	let userId = req.params.id;
+	console.log(userId);
+	db.user_role = db.user_role.filter((u) => u.userId != userId);
+	db.users = db.users.filter((u) => u.id != userId);
+	res.json({
+		code: 1,
+	});
 });
 //删除角色（需要删掉，user_role, role, role_permission）
 server.delete("/api/roles/:id", (req, res, next) => {
-  let roleId = req.params.id;
-  console.log(roleId);
-  db.user_role = db.user_role.filter((u) => u.roleId != roleId);
-  db.roles = db.roles.filter((u) => u.id != roleId);
-  db.role_permission = db.role_permission.filter((u) => u.roleId != roleId);
-  res.json({
-    code: 1,
-  });
+	let roleId = req.params.id;
+	console.log(roleId);
+	db.user_role = db.user_role.filter((u) => u.roleId != roleId);
+	db.roles = db.roles.filter((u) => u.id != roleId);
+	db.role_permission = db.role_permission.filter((u) => u.roleId != roleId);
+	res.json({
+		code: 1,
+	});
 });
 
 //删除角色
 server.get("/test", (req, res, next) => {
-  // let permissionList = JSON.parse(JSON.stringify(db.permissions))
-  //   .map((u) => {
-  //     u.children = [];
-  //     return u;
-  //   })
-  //   .sort((a, b) => a.pId - b.pId);
+	// let permissionList = JSON.parse(JSON.stringify(db.permissions))
+	//   .map((u) => {
+	//     u.children = [];
+	//     return u;
+	//   })
+	//   .sort((a, b) => a.pId - b.pId);
 
-  let result = formatPermissionList(db.permissions);
-  res.json({ data: result });
+	let result = formatPermissionList(db.permissions);
+	res.json({ data: result });
 });
 // Use default router
 server.use(router);
-server.listen(3004, () => {
-  console.log("JSON Server is running, port number is: 3004");
+server.listen(3101, () => {
+	console.log("JSON Server is running, port number is: 3101");
 });
 
 function formatPermissionList(permissionList) {
-  permissionList = JSON.parse(JSON.stringify(permissionList))
-    .map((u) => {
-      u.children = [];
-      return u;
-    })
-    .sort((a, b) => a.pId - b.pId);
-  let res = [];
-  res.push(...permissionList.filter((p) => p.pId == 0));
+	permissionList = JSON.parse(JSON.stringify(permissionList))
+		.map((u) => {
+			u.children = [];
+			return u;
+		})
+		.sort((a, b) => a.pId - b.pId);
+	let res = [];
+	res.push(...permissionList.filter((p) => p.pId == 0));
 
-  let arr = permissionList.filter((p) => p.pId != 0);
-  arr.forEach((val) => {
-    formatPermissionListByParentId(val, res);
-  });
-  return res;
+	let arr = permissionList.filter((p) => p.pId != 0);
+	arr.forEach((val) => {
+		formatPermissionListByParentId(val, res);
+	});
+	return res;
 }
 
 function formatPermissionListByParentId(item, res) {
-  let isFind = false;
-  for (let i = 0; i < res.length; i++) {
-    if (res[i].id === item.pId) {
-      isFind = true;
-      res[i].children.push(item);
-      return;
-    }
-  }
-  if (!false) {
-    for (let i = 0; i < res.length; i++) {
-      if (res[i].children.length > 0) {
-        formatPermissionListByParentId(item, res[i].children);
-      } else {
-        if (res[i].id === item.pId) {
-          res[i].children.push(item);
-          return;
-        }
-      }
-    }
-  }
+	let isFind = false;
+	for (let i = 0; i < res.length; i++) {
+		if (res[i].id === item.pId) {
+			isFind = true;
+			res[i].children.push(item);
+			return;
+		}
+	}
+	if (!false) {
+		for (let i = 0; i < res.length; i++) {
+			if (res[i].children.length > 0) {
+				formatPermissionListByParentId(item, res[i].children);
+			} else {
+				if (res[i].id === item.pId) {
+					res[i].children.push(item);
+					return;
+				}
+			}
+		}
+	}
 }
